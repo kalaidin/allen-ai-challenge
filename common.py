951309e5041737de277162b9740e012be29ff744
Marat Zaynutdinov <tsundokum@gmail.com>
2015-12-27 22:36:35 +0300

    splitted common functions and rnn code

---
+++ common.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import  print_function, division
+
+import numpy as np
+import re
+
+TRAIN_FILE = '/home/marat/Downloads/training_set.tsv'
+TEST_FILE = '/home/marat/Downloads/validation_set.tsv'
+W2V_DICT_FILE = '/home/marat/Downloads/w2v_a2.tsv'
+W2V_DIM = 300
+
+SUBMISSION_FILE = '/home/marat/allen-ai-submission.csv'
+QUESTION_LEN = 50
+ANSWER_MAX_LEN = 20
+
+
+def read_dict():
+    d = {}
+    with open(W2V_DICT_FILE) as f:
+        for row in (line.strip().split('\t') for line in f):
+            w = row[0]
+            v = np.array(row[1:], dtype='float32')
+            assert v.shape == (300,)
+            d[w] = v
+    return d
+
+W2V = read_dict()
+
+
+def text2words(t):
+    s = re.sub(r'[^\w\s]', '', t)
+    r = s.lower().split()
+    return r
+
+
+def text2vec(text, seq_length):
+    vecs = []
+    for w in text2words(text):
+        try:
+            vecs.append(W2V[w][np.newaxis, :])
+        except KeyError:
+            continue
+    if not vecs:
+        vecs.append(np.random.normal(scale=1e-4, size=(1, W2V_DIM)).astype(dtype='float32'))
+    rec = np.concatenate(vecs, axis=0).astype('float32')
+    if rec.shape[0] > seq_length:
+        # trim long sentences
+        rec = rec[rec.shape[0] - seq_length:, :]
+    elif rec.shape[0] < seq_length:
+        # extend short sentences with zeros
+        rec = np.vstack([np.zeros((seq_length - rec.shape[0], rec.shape[1])), rec])
+    assert rec.shape[0] == seq_length
+    return rec
+
+
+def read_data(file_name):
+    data = []
+    with open(file_name) as f:
+        header = f.readline().strip().split('\t')
+        print('Train file header:', ', '.join(header))
+        for row in (line.strip().split('\t') for line in f):
+            i, q, answer, aA, aB, aC, aD = row
+            data.append((int(i),
+                         text2vec(q, QUESTION_LEN),
+                         answer,
+                         text2vec(aA, ANSWER_MAX_LEN),
+                         text2vec(aB, ANSWER_MAX_LEN),
+                         text2vec(aC, ANSWER_MAX_LEN),
+                         text2vec(aD, ANSWER_MAX_LEN)))
+    return data
+
+
+def read_submission_sample(file_name):
+    with open(file_name) as f:
+        header = f.readline().strip().split('\t')
+        print('Submission header:', ', '.join(header))
+        for row in (line.strip().split('\t') for line in f):
+            i, q, aA, aB, aC, aD = row
+            yield (int(i),
+                   text2vec(q, QUESTION_LEN),
+                   text2vec(aA, ANSWER_MAX_LEN),
+                   text2vec(aB, ANSWER_MAX_LEN),
+                   text2vec(aC, ANSWER_MAX_LEN),
+                   text2vec(aD, ANSWER_MAX_LEN))
+
+
+def generate_batches(data):
+    for i, q, a, A, B, C, D in data:
+        correct_index = {'A': 0, 'B': 1, 'C': 2, 'D': 3}[a]
+        correct_answer = [A, B, C, D][correct_index]
+        wrong_answers = [A, B, C, D]
+        del wrong_answers[correct_index]
+        for wa in wrong_answers:
+            yield i, q, correct_answer, wa
+
+
+def zip_arrays(arr1, arr2):
+    assert arr1.shape == arr2.shape
+    n = arr1.shape[0]
+    z = np.zeros((n*2,) + arr1.shape[1:], dtype=arr1.dtype)
+    for i in xrange(arr1.shape[0]):
+        z[i*2] = arr1[i]
+        z[i*2 + 1] = arr2[i]
+    return z
+
+
--- rnn_construct_new_dimension.py
+++ rnn_construct_new_dimension.py
@@ -3,125 +3,31 @@
 
 from __future__ import  print_function, division
 
+from itertools import chain
 from time import time
 
-import numpy as np
 import theano
 import theano.tensor as T
 import lasagne
-import re
 from sklearn.cross_validation import KFold
-
-TRAIN_FILE = '/home/marat/Downloads/training_set.tsv'
-TEST_FILE = '/home/marat/Downloads/validation_set.tsv'
-W2V_DICT_FILE = '/home/marat/Downloads/w2v_a2.tsv'
+from common import *
 
 SUBMISSION_FILE = '/home/marat/allen-ai-submission.csv'
 
-W2V_DIM = 300
 QUESTION_LEN = 50
 ANSWER_MAX_LEN = 20
-BATCH_SIZE = 15
+BATCH_SIZE = 50
 
-OUTPUT_DIM = 100
-N_HIDDEN = 100
+OUTPUT_DIM = 50
+N_HIDDEN = 50
+
 GRAD_CLIP = 10
 NUM_EPOCHS = 100
-MARGIN = 0.1
+MARGIN = np.exp(-1) + 0.1
 
 # TODO: add L2
 # TODO: add dropout
 # TODO: add mask
-
-
-def read_dict():
-    d = {}
-    with open(W2V_DICT_FILE) as f:
-        for row in (line.strip().split('\t') for line in f):
-            w = row[0]
-            v = np.array(row[1:], dtype='float32')
-            assert v.shape == (300,)
-            d[w] = v
-    return d
-
-W2V = read_dict()
-
-
-def text2words(t):
-    s = re.sub(r'[^\w\s]', '', t)
-    r = s.lower().split()
-    return r
-
-
-def text2vec(text, seq_length):
-    vecs = []
-    for w in text2words(text):
-        try:
-            vecs.append(W2V[w][np.newaxis, :])
-        except KeyError:
-            continue
-    if not vecs:
-        vecs.append(np.random.normal(scale=1e-4, size=(1, W2V_DIM)).astype(dtype='float32'))
-    rec = np.concatenate(vecs, axis=0).astype('float32')
-    if rec.shape[0] > seq_length:
-        # trim long sentences
-        rec = rec[rec.shape[0] - seq_length:, :]
-    elif rec.shape[0] < seq_length:
-        # extend short sentences with zeros
-        rec = np.vstack([np.zeros((seq_length - rec.shape[0], rec.shape[1])), rec])
-    assert rec.shape[0] == seq_length
-    return rec
-
-
-def read_data(file_name):
-    data = []
-    with open(file_name) as f:
-        header = f.readline().strip().split('\t')
-        print('Train file header:', ', '.join(header))
-        for row in (line.strip().split('\t') for line in f):
-            i, q, answer, aA, aB, aC, aD = row
-            data.append((int(i),
-                         text2vec(q, QUESTION_LEN),
-                         answer,
-                         text2vec(aA, ANSWER_MAX_LEN),
-                         text2vec(aB, ANSWER_MAX_LEN),
-                         text2vec(aC, ANSWER_MAX_LEN),
-                         text2vec(aD, ANSWER_MAX_LEN)))
-    return data
-
-
-def read_submission_sample(file_name):
-    with open(file_name) as f:
-        header = f.readline().strip().split('\t')
-        print('Submission header:', ', '.join(header))
-        for row in (line.strip().split('\t') for line in f):
-            i, q, aA, aB, aC, aD = row
-            yield (int(i),
-                   text2vec(q, QUESTION_LEN),
-                   text2vec(aA, ANSWER_MAX_LEN),
-                   text2vec(aB, ANSWER_MAX_LEN),
-                   text2vec(aC, ANSWER_MAX_LEN),
-                   text2vec(aD, ANSWER_MAX_LEN))
-
-
-def generate_batches(data):
-    for i, q, a, A, B, C, D in data:
-        correct_index = {'A': 0, 'B': 1, 'C': 2, 'D': 3}[a]
-        correct_answer = [A, B, C, D][correct_index]
-        wrong_answers = [A, B, C, D]
-        del wrong_answers[correct_index]
-        for wa in wrong_answers:
-            yield i, q, correct_answer, wa
-
-
-def zip_arrays(arr1, arr2):
-    assert arr1.shape == arr2.shape
-    n = arr1.shape[0]
-    z = np.zeros((n*2,) + arr1.shape[1:], dtype=arr1.dtype)
-    for i in xrange(arr1.shape[0]):
-        z[i*2] = arr1[i]
-        z[i*2 + 1] = arr2[i]
-    return z
 
 all_data = read_data(TRAIN_FILE)
 train_idx, valid_idx = KFold(len(all_data)).__iter__().next()
@@ -175,8 +81,8 @@ wrong_nvec = wrong_vec / wrong_vec.norm(2, axis=1).dimshuffle((0, 'x'))
 
 print('Computing cost ...')
 
-correct_dist = 1 - T.sum(question_nvec * correct_nvec, axis=1)
-wrong_dist = 1 - T.sum(question_nvec * wrong_nvec, axis=1)
+correct_dist = T.exp(T.sum(question_nvec * correct_nvec, axis=1))
+wrong_dist = T.exp(T.sum(question_nvec * wrong_nvec, axis=1))
 
 cost = T.maximum(0, correct_dist - wrong_dist + MARGIN).mean()
 
@@ -212,16 +118,15 @@ for e in xrange(NUM_EPOCHS):
     print(e, np.mean(train_costs), np.mean(valid_costs), '%.0fsec' % time_passed)
 
 
+# Forming submission file SUBMISSION_FILE
 with open(SUBMISSION_FILE, 'w') as f:
     f.write('id,correctAnswer\n')
     for i, q, A, B, C, D in read_submission_sample(TEST_FILE):
         q = np.repeat(q[np.newaxis], 2, axis=0)
         a = np.vstack([A[np.newaxis], B[np.newaxis], C[np.newaxis], D[np.newaxis]])
-        d = dist_fn(q, a)
-        d_A, d_B = d[0]
-        d_C, d_D = d[1]
-        answer_index = np.argmin([d_A, d_B, d_C, d_D])
+        d = list(chain(*dist_fn(q, a)))
+        answer_index = np.argmin(d)
         answer = 'ABCD'[answer_index]
         text = '%s,%s\n' % (i, answer)
         f.write(text)
-        print(text[:-1], [d_A, d_B, d_C, d_D])
+        print(text[:-1], d)
