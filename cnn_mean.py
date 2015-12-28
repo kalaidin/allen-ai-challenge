44d5db484e4b363bb603796199609dc76ae0e593
Marat Zaynutdinov <tsundokum@gmail.com>
2015-12-28 16:08:37 +0300

    euclidian distance converges better on a validation set

--- cnn_mean.py
+++ cnn_mean.py
@@ -22,12 +22,12 @@ QUESTION_LEN = 50
 ANSWER_MAX_LEN = 20
 BATCH_SIZE = 50
 
-N_HIDDEN = 10
+N_HIDDEN = 100
 OUTPUT_NONLINEARITY = lasagne.nonlinearities.sigmoid
 
-L2 = 1e-5
+L2 = 1e-4
 NUM_EPOCHS = 100
-MARGIN = 0.01
+MARGIN = 1
 
 # TODO: add dropout
 # TODO: add mask
@@ -54,14 +54,12 @@ valid_w = np.array(valid_w, dtype='float32')
 print('Building question network ...')
 q_in = lasagne.layers.InputLayer(shape=(None, QUESTION_LEN, W2V_DIM))
 q_n = lasagne.layers.DimshuffleLayer(q_in, (0, 2, 1))
-# q_n = lasagne.layers.ExpressionLayer(q_n, lambda x: T.exp(x), output_shape='auto')
-# q_n = lasagne.layers.ExpressionLayer(q_n, lambda x: x / x.norm(2, axis=1).dimshuffle((0, 'x', 1)), output_shape='auto')
 q_n = lasagne.layers.Conv1DLayer(q_n, N_HIDDEN, filter_size=1, pad=0)
 # q_n = lasagne.layers.dropout(q_n)
 q_n = lasagne.layers.Conv1DLayer(q_n, 1, filter_size=7, pad=3, nonlinearity=OUTPUT_NONLINEARITY)
 q_out = lasagne.layers.reshape(q_n, shape=([0], -1, 1))
 question_vec = T.sum(lasagne.layers.get_output(q_out) * q_in.input_var, axis=1)
-question_vec = question_vec / (question_vec.norm(2, axis=1).dimshuffle((0, 'x')) + EPS)
+# question_vec = question_vec / (question_vec.norm(2, axis=1).dimshuffle((0, 'x')) + EPS)
 # question_vec.eval({q_in.input_var: train_q[:13]}).shape
 q_params = lasagne.layers.get_all_params(q_out)
 
@@ -70,28 +68,28 @@ lasagne.layers.get_output(q_out).eval({q_in.input_var: train_q[0:1]})
 print('Building siamese answer network ...')
 a_in = lasagne.layers.InputLayer(shape=(None, ANSWER_MAX_LEN, W2V_DIM))
 a_n = lasagne.layers.DimshuffleLayer(a_in, (0, 2, 1))
-# a_n = lasagne.layers.ExpressionLayer(a_n, lambda x: T.exp(x), output_shape='auto')
-# a_n = lasagne.layers.ExpressionLayer(a_n, lambda x: x / x.norm(2, axis=1).dimshuffle((0, 'x', 1)), output_shape='auto')
 a_n = lasagne.layers.Conv1DLayer(a_n, N_HIDDEN, filter_size=1, pad=0)
 # a_n = lasagne.layers.dropout(a_n)
 a_n = lasagne.layers.Conv1DLayer(a_n, 1, filter_size=7, pad=3, nonlinearity=OUTPUT_NONLINEARITY)
 a_out = lasagne.layers.reshape(a_n, shape=([0], -1, 1))
-answer_vec = T.sum(lasagne.layers.get_output(a_out, ) * a_in.input_var, axis=1)
-answer_vec = answer_vec / (answer_vec.norm(2, axis=1).dimshuffle((0, 'x')) + EPS)
+answer_vec = T.sum(lasagne.layers.get_output(a_out) * a_in.input_var, axis=1)
+# answer_vec = answer_vec / (answer_vec.norm(2, axis=1).dimshuffle((0, 'x')) + EPS)
 a_params = lasagne.layers.get_all_params(a_out)
+# lasagne.layers.get_output(a_out).eval({a_in.input_var: train_w[:1]})
+# answer_vec.eval({a_in.input_var: train_c[:1]}).shape
 
 correct_vec = answer_vec[0::2]
 wrong_vec = answer_vec[1::2]
 
 print('Computing cost ...')
 
-correct_cos_sim = T.sum(question_vec * correct_vec, axis=1)
-wrong_cos_sim = T.sum(question_vec * wrong_vec, axis=1)
-# correct_cos_sim = (question_vec-correct_vec).norm(2, axis=1)
-# wrong_cos_sim = (question_vec-wrong_vec).norm(2, axis=1)
+# correct_cos_sim = T.sum(question_vec * correct_vec, axis=1)
+# wrong_cos_sim = T.sum(question_vec * wrong_vec, axis=1)
+correct_cos_sim = (question_vec-correct_vec).norm(2, axis=1)
+wrong_cos_sim = (question_vec-wrong_vec).norm(2, axis=1)
 
 cost_l2 = L2*regularize_network_params(a_out, l2) + L2*regularize_network_params(q_out, l2)
-cost = T.maximum(0, (MARGIN - correct_cos_sim/2 + wrong_cos_sim/2)).mean() + cost_l2
+cost = T.maximum(0, (MARGIN + correct_cos_sim - wrong_cos_sim)).mean() + cost_l2
 # cost = T.maximum(0, (MARGIN - T.sqr(correct_cos_sim) + T.sqr(wrong_cos_sim))).mean() + cost_l2
 
 print('Computing updates ...')
@@ -125,7 +123,7 @@ for e in xrange(NUM_EPOCHS):
         cost = cost_fn(valid_q[keys], valid_answers)
         d = dist_fn(valid_q[keys], valid_answers)
         valid_costs.extend([cost] * keys.shape[0])
-        valid_correct_sum += (d[0] > d[1]).sum()
+        valid_correct_sum += (d[0] < d[1]).sum()
         valid_count += d[0].shape[0]
     valid_accuracy = valid_correct_sum / valid_count * 100
 
